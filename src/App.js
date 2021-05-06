9299888a2dab894e2cab65e07da22bf10903fb19
Banlock-Didier-Roch <Banlockdidierroch@yahoo.com>
2021-05-06 14:36:41 +0100

    B et A

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edits Didier <code>src/App.js</code> and save to reload.
+          Edits BIDIAS et Michèle <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
