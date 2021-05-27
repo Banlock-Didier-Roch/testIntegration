1b53de554f3a78d3f80abeb065df8ac59e99dff1
serena-lydie <lydieserenabaliaba@gmail.com>
2021-05-27 15:02:48 +0200

    premier test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edits BANLOCK <code>src/App.js</code> and save to reload.
+          Edits BALIABA <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
